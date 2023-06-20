16bb695e49bfb40e175f06c3a626ba58ad9876e7
Sagrika Chauhan <you@example.com>
2023-06-20 14:29:11 +0530

    mapped booking data to billing

--- src/main/java/com/example/temporalproducer/mapper/BookingToBillingMapper.java
+++ src/main/java/com/example/temporalproducer/mapper/BookingToBillingMapper.java
@@ -13,6 +13,8 @@ public interface BookingToBillingMapper {
     BookingToBillingMapper INSTANCE = Mappers.getMapper(BookingToBillingMapper.class);
     @Mapping(target = "bookingNumber" , source = "booking.bookingNumber")
     @Mapping(target = "parties" , source = "booking.parties")
+    @Mapping(target = "locations" , source = "servicePlanLegs")
+    @Mapping(target = "equipment" , source = "booking.bookingEquipment")
     FinancialOrderDomain convert(ServicePlan bookingPayload);
 }
 
